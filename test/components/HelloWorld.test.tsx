3924245af51beb92fc4aecc17c9063505eb835c3
Kinga Kazala <22429087+kkazala@users.noreply.github.com>
2023-07-20 14:04:12 +0200

    first commit

---
+++ test/components/HelloWorld.test.tsx
@@ -0,0 +1,15 @@
+﻿import '@testing-library/jest-dom';
+import { RenderResult, render } from '@testing-library/react';
+import React from "react";
+import HelloWorld from "../../src/webparts/helloWorld/components/HelloWorld";
+
+describe("HelloWorld.tsx", () => {
+    it("should render correctly", async () => {
+
+        let helloWorld: RenderResult =  render(<HelloWorld  description="" isDarkTheme={false} environmentMessage="" hasTeamsContext={false} userDisplayName="" />);
+        expect(helloWorld.getByTestId('helloWorld')).toBeInTheDocument();
+
+    });
+});
+
+
